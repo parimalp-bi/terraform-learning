385489a1ccb7744481409d349c8ca87488c46c81
parimalp-bi <parimalp.bi@gmail.com>
2024-12-02 22:18:34 +0530

    updated VM module

--- terraform-azure-vm/modules/network/outputs.tf
+++ terraform-azure-vm/modules/network/outputs.tf
@@ -3,22 +3,7 @@ output "subnet_id" {
   value       = azurerm_subnet.subnet.id
 }
 
-output "address_space" {
-  description = "The address space for the virtual network"
-  value       = var.address_space
-}
-
-output "address_prefix" {
-  description = "The address prefix for the subnet"
-  value       = var.address_prefix
-}
-
-output "subnet_name" {
-  description = "The name of the created subnet"
-  value       = var.subnet_name
-}
-
-output "vnet_name" {
-  description = "The name of the created virtual network"
-  value       = var.vnet_name
+output "vnet_id" {
+  description = "The ID of the virtual network"
+  value       = azurerm_virtual_network.vnet.id
 }
--- terraform-azure-vm/modules/vm/outputs.tf
+++ terraform-azure-vm/modules/vm/outputs.tf
@@ -1,9 +1,4 @@
 output "vm_id" {
-  description = "ID of the created Virtual Machine"
+  description = "The ID of the created virtual machine"
   value       = azurerm_virtual_machine.vm.id
-}
-
-output "nic_id" {
-  description = "ID of the created Virtual Machine"
-  value       = module.nic.nic_id
 }
