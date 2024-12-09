ceb38864afcd8c1b140a7eda6f9eeda01a66e106
parimalp-bi <parimalp.bi@gmail.com>
2024-12-09 14:14:11 +0530

    refactored code

---
+++ Provisioners/vm/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.9"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id                 = ""
+  resource_provider_registrations = "none"
+  tenant_id                       = ""
+
+}
